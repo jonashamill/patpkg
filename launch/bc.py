cbf40fc6be1ad405b50ab21f397e8564414cd250
jay <jonas.hamill@bristol.ac.uk>
2023-07-04 00:34:27 +0100

    adding bc py

---
+++ launch/bc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import rospy
+from ar_track_alvar_msgs.msg import AlvarMarkers
+
+def main():
+    
+    #initialise rosnode
+    rospy.init_node("bc")
+    
+    # create ros pu
+    cmd_pub = rospy.Publisher("", queue_size=1)
+    
+
+    
+     
+    
+    return
+ 
+
+if __name__ == '__main__':
+    main()
