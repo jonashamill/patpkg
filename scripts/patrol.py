3b6a59bf17fba7213ee9816c18aaedc95f295123
jay <jonas.hamill@bristol.ac.uk>
2023-07-04 00:05:21 +0100

    adding all files

---
+++ scripts/patrol.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+#initialise rosnode
+rospy.init_node("patrol")
+
+
+
+def main():
+    
+    
+
+    return
+
+
+if __name__ == '__main__':
+    main()
