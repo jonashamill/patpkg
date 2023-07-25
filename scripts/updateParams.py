4effa7aced4e4a5a6a42f9664ae347c143bc9949
Jay <Jonas.Hamill@bristol.ac.uk>
2023-07-25 14:51:28 +0100

    same

---
+++ scripts/updateParams.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int32
+import time
+
+def plasticCallback(msg):
+
+    plastic = msg.data
+
+    return plastic
+
+def patrolSpeed(plastic):
+
+    patSpeed = rospy.get_param("~initialSpeed", 0.2)
+
+    #plastic =  msg.data # plasticCallback(msg)
+
+    if plastic == 1:
+        patSpeed = patSpeed - 0.02
+    
+    elif plastic == 2:
+        patSpeed = patSpeed + 0.02
+
+    return patSpeed
+
+
+
+def main():
+
+    usePlasticity = rospy.get_param("~usePlasticity", True)
+
+    linearRange = rospy.get_param("~linearRange", 1000)
+    angularRange = rospy.get_param("~angularRange", 500)
+    
+    rospy.loginfo('LinearRange: ', linearRange)
+    rospy.loginfo('angularRange: ', angularRange)
+
+    #initialise rosnode
+    rospy.init_node("patrol")
+    
+    plastic = rospy.Subscriber('plasticTopic', Int32, plasticCallback)
+
+    
+    # create ros pub
+    cmd_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+    
+    while not rospy.is_shutdown():
+
+        
+        if usePlasticity:
+            patSpeed = patrolSpeed(plastic)
+            minPatSpeed = patSpeed - 0.15
+        
+        else:
+            patSpeed = 0.25
+            minPatSpeed = 0.1
+
+
+        rospy.set_param('max_vel_x', patSpeed)
+        rospy.set_param('min_vel_x', minPatSpeed)
+
+           
+
+    return
+
+
+if __name__ == '__main__':
+    main()
