54212030f668acac3204faf3ab017bc8739263a1
Jay <Jonas.Hamill@bristol.ac.uk>
2023-08-01 16:28:56 +0100

    updating plastic to global var

--- scripts/updateParams.py
+++ scripts/updateParams.py
@@ -9,112 +9,20 @@ import os
 import rospkg
 import csv
 
-
-# #Global vars
-# timeList = []
-# minList = []
-# maxList = []
-# start = time.perf_counter()
-# currentMarker = 999
-
-
-# def getTime():
-
-#     #grabbing time and date to provide unique ID for logs
-#     dateTime = datetime.now()
-#     dtString = dateTime.strftime("%Y%m%d%H%M%S") #ISO 8601 Standard
-
-#     return dtString
-
-
-# def getPath():
-
-#     timenow = getTime()
-
-#     rp = rospkg.RosPack()
-#     packagePath = rp.get_path('patpkg')
-
-#     path = os.path.join(packagePath, "logs")
-
-#     fullpath = os.path.join(path, "arlog_" + timenow + ".csv")
-
-#     print (fullpath)
-
-#     return path, fullpath
-
-# def makeFolder():
-
-#     path, _ = getPath()
-
-#     testFile = None
-
-#     # test folder permisions
-#     try:
-#         testFile = open(os.path.join(path, 'test.txt'), 'w+')
-#     except IOError:
-#         try:
-#             os.mkdir(path)
-#         except OSError:
-#             print("No log folder created")
-#         else:
-#             print("Log folder created")
-
-#     else:
-
-#         testFile.close()
-#         os.remove(testFile.name)
-
-# def saveCSV():
-    
-#     _, filename = getPath()
-
-#     with open(filename, "w", newline="") as file:
-#         writer = csv.writer(file)
-#         writer.writerow(['Time', 'Velocity Min', 'Velocity Max'])
-        
-#         for i in range(len(maxList)):
-#             writer.writerow([timeList[i], minList[i], maxList[i]])
-
-
-
-# def checkDuplicate(iterable,check):
-#     for i in iterable:
-#         if i == check:
-#             return True
-
-
-# def getSpeed(msg, patSpeed, minPatSpeed):
-
-#     # for marker in msg.markers:
-#     #     global currentMarker
-
-#     #     if marker.id != currentMarker:
-            
-#     finish = time.perf_counter()
-#     timeTaken = round(finish-start, 5)
-#     # currentMarker = marker.id
-
-
-#     # if checkDuplicate(timeTaken, currentMarker) == True:
-#     #     continue
-#     # else:
-#     timeList.append(timeTaken)
-#     maxList.append(patSpeed)
-#     minList.append(minPatSpeed)
-    
-
-#     rospy.loginfo(currentMarker)
-#     rospy.loginfo(timeTaken)
+plastic = 0
 
 
 def plasticCallback(msg):
+    
+    global plastic
 
     plastic = msg.data
 
     return plastic
 
-def patrolSpeed(plastic, patSpeed):
+def patrolSpeed(patSpeed):
     
+    global plastic
 
     #plastic =  msg.data # plasticCallback(msg)
 
@@ -132,6 +40,7 @@ def patrolSpeed(plastic, patSpeed):
 
 def main():
 
+    global plastic
 
     # msg = getMSG
 
@@ -152,7 +61,7 @@ def main():
 
         
         if usePlasticity:
-            patSpeed = patrolSpeed(plastic, patSpeed)
+            patSpeed = patrolSpeed(patSpeed)
             minPatSpeed = patSpeed - 0.15
             
         
